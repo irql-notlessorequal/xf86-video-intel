irql-notlessorequal/xf86-video-intel

commit d94fbdc233a63ab06ee8b7efacc7079c8d6b75cc

sna: Initial asynchronous page-flip fixes

src/sna/sna_display.c
@@ -7276,7 +7276,18 @@ retry_flip:
 		DBG(("%s: crtc %d id=%d, pipe=%d  --> fb %d\n",
 		     __FUNCTION__, i, __sna_crtc_id(crtc), __sna_crtc_pipe(crtc), arg.fb_id));
 		if (drmIoctl(sna->kgem.fd, DRM_IOCTL_MODE_PAGE_FLIP, &arg)) {
-			ERR(("%s: pageflip failed with err=%d\n", __FUNCTION__, errno));
+			/*
+			 * Avoid printing to Xorg log as this will happen often due to Intel stupidity.
+			 */
+			if (errno == EINVAL && async) {
+				ERR(("%s: pageflip failed with %d, attempting a synchronous fallback...\n", __FUNCTION__, errno));
+				arg.flags &= DRM_MODE_PAGE_FLIP_ASYNC;
+				async = false;
+
+				goto retry_flip;
+			}
+
+			ERR(("%s: pageflip failed with err=%d async=%d\n", __FUNCTION__, errno, async));
 
 			if (errno == EBUSY) {
 				struct drm_mode_crtc mode;

commit 15dc2f806c68db96bb2afca5f4b4f2eec8b8def6

sna: Rewrite the asynchronous page flip code.
Previously, we would fix it by falling back to synchronous flips, this was great in concept, bad in execution.

I have no clue what Xorg was cooking but it would do some questionable things and introduce intermittent display delay
when doing fallback, on top of being limited to your display's refresh rate when falling back.

The new code fixes both issues, by simply refusing (for now) to scanout directly LINEAR buffers asynchronously.

Luckily it seems that gets SNA to use the BLT engine to do the conversion for free.

Future work:
- Try to get sna_pixmap_change_tiling to play nice
- Figure out what to do at high resolutions/refresh rates, where the BLT engine is likely not able to keep up.

Fixes: d94fbdc233a63ab06ee8b7efacc7079c8d6b75cc ("sna: Initial asynchronous page-flip fixes")
Fixes: cde075c111cffe5f0ca5610a7cfd2b6ee3fea718 ("sna: asynchronous page-flip fixes")
Fixes: 39ccf08e1cd534fcee0b3ce946a6a7227b0a4a33 ("sna: asynchronous page-flip fixes (part 2)")

src/sna/sna.h
@@ -295,8 +295,6 @@ struct sna {
 	bool enable_async_swap : 1;
 	/* Enables an optimization that probably doesn't even work at all still. */
 	bool enable_reduced_flushing : 1;
-	/* Used to defer waiting for scanline during an already synchronous pageflip. */
-	bool page_flip_workaround_active : 1;
 
 	unsigned watch_shm_flush;
 	unsigned watch_dri_flush;

src/sna/sna_blt.c
@@ -2552,6 +2552,7 @@ sna_blt_composite(struct sna *sna,
 #if DEBUG_NO_BLT || NO_BLT_COMPOSITE
 	return false;
 #endif
+
 	DBG(("%s (%d, %d), (%d, %d), %dx%d\n",
 	     __FUNCTION__, x, y, dst_x, dst_y, width, height));
 

src/sna/sna_display.c
@@ -7222,6 +7222,7 @@ sna_page_flip(struct sna *sna,
 
 		arg.crtc_id = __sna_crtc_id(crtc);
 		arg.fb_id = get_fb(sna, bo, width, height);
+
 		if (arg.fb_id == 0) {
 			assert(count == 0);
 			break;
@@ -7259,8 +7260,9 @@ update_scanout:
 					goto next_crtc;
 
 				/* queue a flip in order to send the event */
-			} else
+			} else {
 				goto error;
+			}
 		}
 
 		/* Only the reference crtc will finally deliver its page flip
@@ -7276,34 +7278,6 @@ retry_flip:
 		DBG(("%s: crtc %d id=%d, pipe=%d  --> fb %d\n",
 		     __FUNCTION__, i, __sna_crtc_id(crtc), __sna_crtc_pipe(crtc), arg.fb_id));
 		if (drmIoctl(sna->kgem.fd, DRM_IOCTL_MODE_PAGE_FLIP, &arg)) {
-			/*
-			 * Intel introduced asynchronous page-flipping support into the kernel in late 2022.
-			 * This is only useful if you are dealing with Intel hardware only [1] or have
-			 * a Gen 12+ iGPU, which obviously isn't supported with SNA/UXA.
-			 * 
-			 * Sadly, on legacy hardware using the LINEAR modifier for direct PRIME scanout isn't
-			 * supported and the kernel will yell at us with a EINVAL. 
-			 *
-			 * Try once more (synchronously) to workaround potential hangs that otherwise occur.
-			 *
-			 * Avoid printing to Xorg log as this will happen often.
-			 * 
-			 * [1] Intel modifiers vary between generations but most support X and Y tiling, so we can just reuse them,
-			 * otherwise we suffer with LINEAR.
-			 */
-			if (errno == EINVAL && async) {
-				DBG(("%s: pageflip failed with err=%d, attempting a synchronous fallback...\n", __FUNCTION__, errno));
-				arg.flags ^= DRM_MODE_PAGE_FLIP_ASYNC;
-				async = false;
-
-				/**
-				 * Avoid synchronizing to vblank since we're about to do this anyways...
-				 */
-				sna->page_flip_workaround_active = true;
-
-				goto retry_flip;
-			}
-
 			ERR(("%s: pageflip failed with err=%d async=%d\n", __FUNCTION__, errno, async));
 
 			if (errno == EBUSY) {
@@ -7366,12 +7340,6 @@ next_crtc:
 		count++;
 	}
 	sigio_unblock(sigio);
-
-	/* We've had a successful pageflip, return to previous behaviour. */
-	if (sna->page_flip_workaround_active)
-	{
-		sna->page_flip_workaround_active = false;
-	}
 
 	DBG(("%s: page flipped %d crtcs\n", __FUNCTION__, count));
 	return count;
@@ -8605,9 +8573,6 @@ sna_wait_for_scanline(struct sna *sna,
 	assert(pixmap == sna->front);
 
 	if (sna->flags & SNA_NO_VSYNC)
-		return false;
-
-	if (sna->page_flip_workaround_active)
 		return false;
 
 	/*

src/sna/sna_present.c
@@ -786,7 +786,7 @@ flip(struct sna *sna,
 }
 
 static struct kgem_bo *
-get_flip_bo(PixmapPtr pixmap)
+get_flip_bo(PixmapPtr pixmap, bool async_flip)
 {
 	struct sna *sna = to_sna_from_pixmap(pixmap);
 	struct sna_pixmap *priv;
@@ -854,6 +854,27 @@ get_flip_bo(PixmapPtr pixmap)
 		return NULL;
 	}
 
+	/*
+	 * Intel introduced asynchronous page-flipping support into the kernel in late 2022.
+	 * This is only useful if you are dealing with Intel hardware only [1] or have
+	 * a Gen 12+ iGPU, which obviously isn't supported with SNA/UXA.
+	 * 
+	 * Sadly, on legacy hardware using the LINEAR modifier for direct PRIME scanout isn't
+	 * supported and the kernel will yell at us with a EINVAL. 
+	 * 
+	 * [1] Intel modifiers vary between generations but most support X and Y tiling, so we can just reuse them,
+	 * otherwise we suffer with LINEAR.
+	 */
+	if (async_flip && priv->gpu_bo->tiling == I915_TILING_NONE)
+	{
+		/**
+		 * TODO(irql): Figure out if it's possible to convert over to TILING_X or TILING_Y by calling sna_pixmap_change_tiling.
+		 * Currently calling it will simply error out.
+		 */
+		DBG(("%s: cannot scanout LINEAR via hardware, forcing BLT+SCANOUT.\n", __FUNCTION__));
+		return NULL;
+	}
+
 	return priv->gpu_bo;
 }
 
@@ -900,7 +921,7 @@ sna_present_flip(RRCrtcPtr crtc,
 			return FALSE;
 	}
 
-	bo = get_flip_bo(pixmap);
+	bo = get_flip_bo(pixmap, !sync_flip);
 	if (bo == NULL) {
 		DBG(("%s: flip invalid bo\n", __FUNCTION__));
 		return FALSE;
@@ -941,7 +962,7 @@ notify:
 		return;
 	}
 
-	bo = get_flip_bo(screen->GetScreenPixmap(screen));
+	bo = get_flip_bo(screen->GetScreenPixmap(screen), false);
 
 	/* Are we unflipping after a failure that left our ScreenP in place? */
 	if (!sna_needs_page_flip(sna, bo))