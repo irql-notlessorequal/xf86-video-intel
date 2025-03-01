irql-notlessorequal/xf86-video-intel

commit 027416ab3551fac1b7e22c7e253c8134c2f222b7

sna/display: Actually disable async-flipping on tiling mismatch.
I am still somehow hitting this path...how?!?!?!

src/sna/sna_display.c
@@ -7288,6 +7288,8 @@ update_scanout:
 			xf86DrvMsg(sna->scrn->scrnIndex, X_ERROR,
 				"%s blocked the driver from switching modifiers during an async page flip, tell a developer NOW!\n", __FUNCTION__);
 			xf86DrvMsg(sna->scrn->scrnIndex, X_DEBUG, "%s: attempted tiling: %d, CRTC tiling: %d\n", __FUNCTION__, bo->tiling, crtc->bo->tiling);
+			
+			sna->flags &= SNA_HAS_ASYNC_FLIP;
 			goto error;
 		}
 
@@ -7343,7 +7345,6 @@ error:
 			if (count || crtc->bo == bo)
 				sna_mode_restore(sna);
 
-			sna->flags &= ~(data ? SNA_HAS_FLIP : SNA_HAS_ASYNC_FLIP);
 			count = 0;
 			break;
 		}

commit cff2b16267597ccaeed6be6b8035f001f08f7a90

sna/present: Try to actually fix the modifier mismatch
Turns out that whatever demented thing DXVK is doing to trigger this in Xorg, it made things bad.

Instead, revert to the old logic during flip() but make the check no longer depend on an asynchronous flip,
which is no longer the case for some reason... Instead make the check a simple, are we linear, is the user
not requesting a linear FB and is the pixmap from the hellscape known as DRI3.

I wish the Linux desktop stack didn't suck so much.

p.s. We might need to also include the check in check_flip2() but that's for future work.

Fixes: 027416ab3551fac1b7e22c7e253c8134c2f222b7

src/sna/sna.h
@@ -1489,4 +1489,24 @@ static inline bool prefer_y_tiling_scanout(struct sna *sna)
 	return should_prefer_y_scanout;
 }
 
+static inline char* tiling_to_str(int tiling)
+{
+	switch (tiling)
+	{
+		case I915_TILING_NONE:
+			return "I915_TILING_NONE";
+		case I915_TILING_X:
+			return "I915_TILING_X";
+		case I915_TILING_Y:
+			return "I915_TILING_Y";
+
+		default:
+		{
+			char buf[16];
+			snprintf(buf, sizeof (buf), "%d", tiling);
+			return buf;
+		}
+	}
+}
+
 #endif /* _SNA_H */

src/sna/sna_display.c
@@ -7283,13 +7283,12 @@ update_scanout:
 		 * 
 		 * The kernel DOES NOT allow commiting different modifiers.
 		 */
-		if (unlikely(async && bo->tiling != crtc->bo->tiling))
+		if (async && unlikely(bo->tiling != crtc->bo->tiling))
 		{
 			xf86DrvMsg(sna->scrn->scrnIndex, X_ERROR,
-				"%s blocked the driver from switching modifiers during an async page flip, tell a developer NOW!\n", __FUNCTION__);
-			xf86DrvMsg(sna->scrn->scrnIndex, X_DEBUG, "%s: attempted tiling: %d, CRTC tiling: %d\n", __FUNCTION__, bo->tiling, crtc->bo->tiling);
-			
-			sna->flags &= SNA_HAS_ASYNC_FLIP;
+				"%s tried switching modifiers during an async page flip, tell a developer NOW!\n", __FUNCTION__);
+			xf86DrvMsg(sna->scrn->scrnIndex, X_DEBUG, "%s: attempted tiling: %s, CRTC tiling: %s\n", __FUNCTION__, 
+				tiling_to_str(bo->tiling), tiling_to_str(crtc->bo->tiling));
 			goto error;
 		}
 
@@ -7345,6 +7344,7 @@ error:
 			if (count || crtc->bo == bo)
 				sna_mode_restore(sna);
 
+			sna->flags &= ~(data ? SNA_HAS_FLIP : SNA_HAS_ASYNC_FLIP);
 			count = 0;
 			break;
 		}

src/sna/sna_present.c
@@ -678,24 +678,6 @@ static Bool sna_present_check_flip2(RRCrtcPtr crtc, WindowPtr window, PixmapPtr 
 			     __FUNCTION__, flip->gpu_bo->pitch));
 			return FALSE;
 		}
-
-		/**
-		 * Intel introduced asynchronous page-flipping support into the kernel in late 2022.
-		 * This is only useful if you are dealing with Intel hardware only [1] or have
-		 * a Gen 12+ iGPU, which obviously isn't supported with SNA/UXA.
-		 * 
-		 * Sadly, on legacy hardware using the LINEAR modifier for direct PRIME scanout isn't
-		 * supported and the kernel will yell at us with a EINVAL. 
-		 * 
-		 * [1] Intel modifiers vary between generations but most support X and Y tiling, so we can just reuse them,
-		 * otherwise we suffer with LINEAR.
-		 */
-		if (!sync_flip && flip->gpu_bo->tiling == I915_TILING_NONE)
-		{
-			DBG(("%s: pinned bo, cannot scanout LINEAR via hardware.\n", __FUNCTION__));
-			*reason = PRESENT_FLIP_REASON_BUFFER_FORMAT;
-			return FALSE;
-		}
 	}
 
 	return TRUE;
@@ -869,6 +851,19 @@ get_flip_bo(PixmapPtr pixmap, bool async_flip)
 
 	if (priv->gpu_bo->pitch & 63) {
 		DBG(("%s: invalid pitch, no conversion\n", __FUNCTION__));
+		return NULL;
+	}
+
+	/**
+	 * This is horribly convoluted but we need to prevent it from
+	 * 1. Breaking asynchronous page flipping between foreign GPUs.
+	 * 2. Switching the framebuffer modifier to LINEAR since that's dumb.
+	 */
+	if (priv->gpu_bo->tiling == I915_TILING_NONE
+		 && !(sna->flags & SNA_LINEAR_FB)
+		 && (priv->pinned & PIN_DRI3))
+	{
+		DBG(("%s: cannot scanout LINEAR via hardware, forcing BLT+SCANOUT.\n", __FUNCTION__));
 		return NULL;
 	}
 